c2ee053cfa05d09131e50ee355b4356183e899e8
Govind8006 <govindaggarwal8006@gmail.com>
2023-02-15 22:35:52 +0530

    first commit

---
+++ array/unionOfSet.java
@@ -0,0 +1,65 @@
+package array;
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+class unionOfSet {
+    public static void main(String[] args) {
+
+        try (// Taking input using class Scanner
+                Scanner sc = new Scanner(System.in)) {
+            // Taking total count of testcases
+            int t = sc.nextInt();
+            sc.nextLine();
+            while (t-- > 0) {
+
+                int n, m;
+
+                // taking size of array a
+                n = sc.nextInt();
+
+                // taking size of array b
+                m = sc.nextInt();
+
+                // Creating 2 array of size n and m
+                int a[] = new int[n];
+                int b[] = new int[m];
+
+                // inserting elements to array a
+                for (int i = 0; i < n; i++) {
+                    a[i] = sc.nextInt();
+                }
+
+                // inserting elements to array b
+                for (int i = 0; i < m; i++) {
+                    b[i] = sc.nextInt();
+                }
+                // calling doUnion method and printing the results
+                System.out.println(Solution.doUnion(a, n, b, m));
+            }
+        }
+
+    }
+}
+// } Driver Code Ends
+
+// User function Template for Java
+
+class Solution {
+    public static int doUnion(int a[], int n, int b[], int m) {
+        // Main logic
+        HashSet<Integer> s = new HashSet<>();
+        for (int i = 0; i < n; i++)
+            s.add(a[i]);
+        for (int i = 0; i < m; i++)
+            s.add(b[i]);
+
+        int number = s.size();
+
+        System.out.println(s);
+
+        return number;
+    }
+}
